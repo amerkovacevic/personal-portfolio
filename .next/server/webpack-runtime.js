968cb115d58d399f37efb7b5084801b3cdb22d01
amerkovacevic <amerkovacevic99@gmail.com>
2025-12-26 18:28:14 -0600

    colors

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2004c7c55c899c75")
+/******/ 		__webpack_require__.h = () => ("2192851f7596a48e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1d691773cf1321e8"; }
+/******/ 		__webpack_require__.h = function() { return "39be339bd844b938"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
